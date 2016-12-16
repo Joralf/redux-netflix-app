d1c326da7ac47a758130d1146627df6e38cea6df
Joralf Quist <j.quist@meteovista.nl>
2016-12-16 13:24:07 +0100

    add styles loaders for webpack

--- source/component.jsx
+++ source/component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './component.scss';
 
 class Component extends React.Component {
   render() {
-    return (<h1>Hellso</h1>);
+    return (<h1 className="component">Hello</h1>);
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: { presets: [ 'es2015', 'react' ] }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },

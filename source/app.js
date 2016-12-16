fc1dcd16f26bc004f67b6f96c4049f48927cd3d5
Joralf Quist <j.quist@meteovista.nl>
2016-12-16 13:13:30 +0100

    initial setup with basic development and production commands

---
+++ source/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Component from './component.jsx';
+
+render(
+  <Component />,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './source/app.js',
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: { presets: [ 'es2015', 'react' ] }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require('html-webpack-template'),
+      appMountId: 'app'
+    })]
+};

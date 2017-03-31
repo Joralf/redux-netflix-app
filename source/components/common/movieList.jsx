8cc4b787e29eff73a48d8f160fd5ee1c1cfec0e2
Joralf Quist <j.quist@meteovista.nl>
2017-03-31 22:59:42 +0200

    add h3 to both results and favorites column

--- source/components/common/movieList.jsx
+++ source/components/common/movieList.jsx
@@ -2,18 +2,23 @@ import React, { PropTypes } from 'react';
 import MovieItem from './movieItem.jsx';
 import './movieList.scss';
 
-const MovieList = ({ movies, buttonText, onClick }) => (
-  <ul>
-    {movies.map(movie =>
-      <MovieItem
-        movie={movie}
-        key={movie.unit}
-        buttonText={buttonText}
-        onClick={() => onClick(movie)}
-      />
-    )}
-  </ul>
-);
+const MovieList = ({ movies, buttonText, titleText, onClick }) => {
+  return (
+    <div>
+      <h3>{titleText}</h3>
+      <ul>
+        {movies.map(movie =>
+          <MovieItem
+            movie={movie}
+            key={movie.unit}
+            buttonText={buttonText}
+            onClick={() => onClick(movie)}
+          />
+        )}
+      </ul>
+    </div>
+  );
+};
 
 MovieList.propTypes = {
   movies: PropTypes.arrayOf(PropTypes.shape({
@@ -22,6 +27,7 @@ MovieList.propTypes = {
     show_title: PropTypes.string.isRequired,
   })),
   onClick: React.PropTypes.func.isRequired,
+  titleText: React.PropTypes.string.isRequired,
   buttonText: React.PropTypes.string.isRequired,
 };
 
--- source/container/resultsContainer.js
+++ source/container/resultsContainer.js
@@ -5,7 +5,8 @@ import MovieList from '../components/common/movieList.jsx';
 const mapStateToProps = (state, ownProps) => {
   return {
     movies: state.search.results,
-    buttonText: "Save to Favorites"
+    buttonText: "Save to Favorites",
+    titleText: "Results",
   };
 };
 
